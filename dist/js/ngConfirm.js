49a241ac8c3f6ab15c9159e4cde5d3827053cf29
imdoge <imdoge2015@gmail.com>
2016-08-10 16:49:43 +0800

    Update ngConfirm.js

--- dist/js/ngConfirm.js
+++ dist/js/ngConfirm.js
@@ -2,20 +2,23 @@ angular.module('ngConfirm', [])
     .directive('confirm', function () {
         return {
             scope: true,
-            controller: ['$scope', '$element', '$attrs', '$compile', '$templateRequest', function ($scope, $element, $attrs, $compile, $templateRequest) {
+            controller: ['$scope', '$element', '$attrs', '$compile', '$animate', '$templateRequest', 
+            function ($scope, $element, $attrs, $compile, $animate, $templateRequest) {
+                var appendToElement = angular.element(document).find('body'),
+                    domEl;
                 var openConfirmBox = (function () {
                     if ($attrs.template) {
                         return function () {
                             $templateRequest($attrs.template).then(function (template) {
-                                var html = $compile(template)($scope);
-                                angular.element(document).find('body').append(html);
+                                domEl = $compile(template)($scope);
+                                $animate.enter(domEl, appendToElement);
                             });
                         }
                     } else {
                         return function () {
                             var template = '<div class="alertify"><div class="dialog"><div><p class="msg">{{ctrl.msg}}</p><nav><button class="cancel btn btn-default" ng-click="ctrl.cancel()">Cancel</button><button class="ok btn btn-primary" ng-click="ctrl.ok()">Ok</button></nav></div></div></div>';
-                            var html = $compile(template)($scope);
-                            angular.element(document).find('body').append(html);
+                            domEl = $compile(template)($scope);
+                            $animate.enter(domEl, appendToElement);
                         }
                     }
                 })();
@@ -27,12 +30,12 @@ angular.module('ngConfirm', [])
                 };
 
                 this.ok = function () {
-                    angular.element(document.querySelector('.alertify')).remove();
+                    $animate.leave(domEl);
                     $attrs.ok && $scope.$parent.$eval($attrs.ok);
                 };
 
                 this.cancel = function () {
-                    angular.element(document.querySelector('.alertify')).remove();
+                    $animate.leave(domEl);
                     $attrs.cancel && $scope.$parent.$eval($attrs.cancel);
                 }
             }],
